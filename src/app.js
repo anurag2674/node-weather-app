a595b149e062b3db364db779b13c2315230a2720
lucifer12674 <g.anuragbobby@gmail.com>
2020-09-25 14:56:45 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const forecast=require('./utils/forecast')
 const express=require('express')
 const hbs=require('hbs')
 const app=express()
+const port=process.env.PORT || 3000
+
 //Define paths for to Expree=ss config
 const publicDirectoryPath=path.join(__dirname,'../public')
 const viewPath=path.join(__dirname,'../templates/views')
@@ -85,6 +87,6 @@ app.get('*',(req,res)=>{
         name:"Anurag Giddalur"
     })
 })
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000")
+app.listen(port,()=>{
+    console.log("Server is up on port "+port)
 })
